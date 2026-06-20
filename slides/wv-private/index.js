8c2d289ae2fa38117b382563c8b16dc218721373
praewpakyada-boop <praewpakyada@gmail.com>
2026-06-20 19:35:52 +0700

    Add WV Private slide component exports

---
+++ slides/wv-private/index.js
@@ -0,0 +1,4 @@
+export { default as BigQuoteSlide } from './BigQuoteSlide.jsx';
+export { default as ComparisonSlide } from './ComparisonSlide.jsx';
+export { default as SectionDividerSlide } from './SectionDividerSlide.jsx';
+export { default as TitleSlide } from './TitleSlide.jsx';
